5bc1101b3252a9037cf0062ac4e3ebe47f6b303d
UGSNBandara <sulithanb119@gmail.com>
2026-02-28 13:23:14 +0530

    fix the mongo URL failing in ci.yml

--- Backend/api/src/test/java/com/adaptivebp/ApiApplicationTests.java
+++ Backend/api/src/test/java/com/adaptivebp/ApiApplicationTests.java
@@ -3,7 +3,9 @@ package com.adaptivebp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.data.mongodb.uri=mongodb://localhost:27017/adaptivebp"
+})
 class ApiApplicationTests {
 
 	@Test
